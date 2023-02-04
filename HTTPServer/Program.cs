d894cbb13cb3593bc06ec0b81995a4ce943f9fb2
Andrey Mihaylov <cjcbcrf322@gmail.com>
2023-02-05 02:28:52 +0300

    Добавьте файлы проекта.

---
+++ HTTPServer/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HTTPServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
